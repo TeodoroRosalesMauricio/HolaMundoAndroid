06f9f3059bd7d0b33199a14b46a7fa075ee73636
TeodoroRosalesMauricio <teodoro.rosales.mauriciio@gmail.com>
2021-11-05 21:00:04 -0600

    A

--- app/src/main/java/com/mauricio/teodoro/rosales/MainActivity.java
+++ app/src/main/java/com/mauricio/teodoro/rosales/MainActivity.java
@@ -3,6 +3,7 @@ package com.mauricio.teodoro.rosales;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Suma(View miView){
+
+    }
+
+    public void Resta(View miView){
+
+    }
+
+    public void Multiplicacion(View miView){
+
+    }
+
+    public void Divicion(View miView){
+
     }
 }
